8f09a9455d095509bf3e7d6fa973f2df3bfe503c
mijkenator <mijkenator@gmail.com>
2016-07-29 15:44:48 +0000

    login form working

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,6 +2,10 @@ defmodule Elapi.AuthController do
   use Elapi.Web, :controller
   require Logger
 
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  alias Elapi.User
+  alias Elapi.Session
 
   def index(conn, _params, _current_user, _claims) do
     render conn, "index.html"
@@ -21,13 +25,44 @@ defmodule Elapi.AuthController do
   end
 
   def ddologin(conn, _params) do
-    Logger.debug("\n AUTH LOGIN #{inspect conn}")
+    Logger.debug("\n AUTH LOGIN1 #{inspect conn}")
     render conn, "callback.html"
   end
 
-  def ddologin(conn, _params, _cu, _cl) do
-    Logger.debug("\n AUTH LOGIN #{inspect conn}")
-    render conn, "callback.html"
+  def ddologin(conn, %{"username" => email, "password" => password} = _params, _cu, _cl) do
+    user = Repo.get_by(User, email: email)
+    cond do
+      user && checkpw(password, user.password_hash) ->
+        sdata = %{
+            ip: Enum.join(Tuple.to_list(conn.remote_ip), "."),
+            test: "lalalal"
+        }
+        session_changeset = Session.create_changeset(%Session{}, %{user_id: user.id, data: sdata})
+        {:ok, session} = Repo.insert(session_changeset)
+        new_conn = Guardian.Plug.sign_in(conn, user)
+        jwt    = Guardian.Plug.current_token(new_conn)
+        {:ok, claims} = Guardian.Plug.claims(new_conn)
+        exp = Map.get(claims, "exp")
+
+        new_conn
+        |> put_flash(:info, "Authentication succeed")
+        |> put_status(:created)
+        |> put_resp_header("authorization", "Bearer #{jwt}")
+        |> put_resp_header("x-expires", to_string(exp))
+        |> render("callback.html", session: session)
+
+      user ->
+        conn
+        |> put_flash(:error, "Authentication failed")
+        |> put_status(:unauthorized)
+        |> render("index.html")
+      true ->
+        dummy_checkpw
+        conn
+        |> put_flash(:error, "Authentication failed")
+        |> put_status(:unauthorized)
+        |> render("index.html")
+    end
   end
 
 end
